30b350efc1ef298de711a916893ff8f92455468b
Sol <solzito.1889@gmail.com>
2024-10-18 13:09:37 -0300

    cambio de logo y correcciones en header

--- Front-End/bytech/src/components/header/Header.tsx
+++ Front-End/bytech/src/components/header/Header.tsx
@@ -13,17 +13,15 @@ export const Header = () => {
     <>
       <div className="grid grid-cols-4 gap-4 bg-primary mt-0  place-items-center">
         <div className="p-2 mx-2">
-          <img alt="imagen del logo" src="/logo.png" width={60} height={60} />
+          <img alt="imagen del logo" src="/logo.png" width={100} height={100} />
         </div>
-        <div className="col-span-2 justify-around lg:flex sm:max-lg:hidden">
+        <div className="col-span-2 justify-around lg:flex sm:max-lg:hidden gap-2 flex-wrap">
           <div className="flex gap-6 items-center text-background">
             <Link href="/products">Productos</Link>
-
-            <Link href="/about">Sobre nosotros</Link>
-
+            <Link href="/about">Sobre Nosotros</Link>
             <Link href="contact">Contacto</Link>
           </div>
-          <div className="mx-4 lg:block md:hidden">
+          <div className=" lg:block md:hidden">
             <input
               type="text"
               name="search"
@@ -35,15 +33,17 @@ export const Header = () => {
             </button>
           </div>
         </div>
-        <div className=" block text-4xl sm:max-lg:hidden ">
-          <RxAvatar color="white"></RxAvatar>
+        <div className="block text-4xl sm:max-lg:hidden col-end-5">
+          <Link href="/login">
+            <RxAvatar color="white"></RxAvatar>
+          </Link>
         </div>
 
         {/* Menú hamburguesa */}
-        <div className="container mx-auto col-span-2 flex justify-end h-full py-4 px-4 gap-4 lg:hidden md:flex place-items-center">
+        <div className="container mx-auto col-end-5 col-span-2 flex justify-end h-full py-4 px-4 gap-4 lg:hidden md:flex place-items-center">
           <div className="relative inline-block text-left">
             <button onClick={() => setIsVisible(!IsVisible)}>
-              <GiHamburgerMenu className="size-8" color="white" />
+              <GiHamburgerMenu className="size-9" color="white" />
             </button>
             {IsVisible && (
               <div
@@ -54,14 +54,12 @@ export const Header = () => {
               >
                 <div className="flex flex-col flex-wrap gap-2 text-white divide-y divide-secondary">
                   <Link href="/products">Productos</Link>
-                  <Link href="/about">Sobre nosotros</Link>
+                  <Link href="/about">Sobre Nosotros</Link>
                   <Link href="contact">Contacto</Link>
+                  <Link href="/login">Inicio Sesion</Link>
                 </div>
               </div>
             )}
-          </div>
-          <div className="text-4xl">
-            <RxAvatar color="white"></RxAvatar>
           </div>
         </div>
       </div>
