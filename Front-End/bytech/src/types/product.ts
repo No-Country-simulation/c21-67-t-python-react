7fd95d33b8d73ec8511adf439f7a9f16a6e76936
Sol <solzito.1889@gmail.com>
2024-10-24 14:54:43 -0300

    propiedades de producto

---
+++ Front-End/bytech/src/types/product.ts
@@ -0,0 +1,11 @@
+export interface propsProduct {
+  id: number;
+  name: string;
+  description: string;
+  image?: string;
+  price: string;
+  stock: number;
+  created_at: string;
+  updated_at: string;
+  category_id: number;
+}
