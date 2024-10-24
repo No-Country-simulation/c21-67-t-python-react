cf9560204a2ab8f5ad377459518c8f47c263340a
Sol <solzito.1889@gmail.com>
2024-10-24 14:58:31 -0300

    mejoras en el body. se agrega lista de productos

--- Front-End/bytech/src/components/body/Body.tsx
+++ Front-End/bytech/src/components/body/Body.tsx
@@ -2,6 +2,7 @@
 import React from "react";
 import Link from "next/link";
 import { CategoryList } from "../category/card/CategoryList";
+import { ProductList } from "../product/ProductList";
 
 const Body = () => {
   return (
@@ -9,25 +10,25 @@ const Body = () => {
       <div>
         <CategoryList />
       </div>
-      <div>
-        <div className="flex justify-center items-center p-6 mb-6">
-          <p className="font-light italic mt-5 text-neutral-500 text-center text-lg">
-            <a className="font-bold italic text-xl">"</a>
-            Innovación y Tecnología al Alcance de Todos
-            <a className="font-bold italic text-xl">"</a>
-          </p>
+      <div className="flex justify-center items-center p-6">
+        <p className="font-light italic mt-2 text-footer text-center text-lg">
+          <a className="font-medium italic text-xl">"</a>
+          Innovación y Tecnología al Alcance de Todos
+          <a className="font-medium italic text-xl">"</a>
+        </p>
+      </div>
+      <div className="mb-5">
+        <div>
+          <ProductList />
         </div>
-      </div>
-      <div className="mb-6">
-        <h1 title="NUESTRA PROPUESTA" color="beige"></h1>
-      </div>
-
-      <div className="flex justify-center mb-5 gap-4">
-        <Link href="/products">
-          <button className="bg-secondary hover:bg-btnActive hover:text-foreground text-white font-medium text-sm mt-10 py-1.5 px-16 rounded-sm sm:px-36 lg:px-80">
+        <div className="flex justify-self-center items-center">
+          <Link
+            href="/products"
+            className=" bg-secondary hover:bg-btnActive hover:text-foreground text-white font-medium text-sm mt-10 py-1.5 px-16 rounded-sm sm:px-36 lg:px-80"
+          >
             VER TODOS
-          </button>
-        </Link>
+          </Link>
+        </div>
       </div>
     </>
   );
