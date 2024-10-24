b074ab26346919b5f978ede4ac6676fe739216ca
Sol <solzito.1889@gmail.com>
2024-10-24 14:55:48 -0300

    card para producto

---
+++ Front-End/bytech/src/components/product/CardBase.tsx
@@ -0,0 +1,24 @@
+import { propsProduct } from "@/types/product";
+import Link from "next/link";
+
+export const CardBase = ({ id, image, name }: propsProduct) => {
+  return (
+    <div
+      key={id}
+      className="flex flex-col justify-center items-center bg-background rounded-lg drop-shadow-lg relative"
+    >
+      <p className="absolute text-primary font-medium text-lg p-2 top-2">
+        {name}
+      </p>
+      <img alt="" src={image} height={300} width={300} />
+      <div className="absolute bottom-2">
+        <Link
+          href={`product/${id}`}
+          className="bg-secondary hover:bg-btnActive hover:text-foreground text-white rounded-sm py-1.5 px-16"
+        >
+          Ver Detalles
+        </Link>
+      </div>
+    </div>
+  );
+};
