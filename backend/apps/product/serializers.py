2028fce624b7889486b9ddce368a047912b04c29
Leandro Sánchez <103838718+leanmsan@users.noreply.github.com>
2024-10-29 19:00:10 -0300

    Se agregó documentación, y se corrigieron errores
    Signed-off-by: Leandro Sánchez <103838718+leanmsan@users.noreply.github.com>

--- backend/apps/product/serializers.py
+++ backend/apps/product/serializers.py
@@ -4,6 +4,9 @@ from apps.category.models import Category
 from .models import Product, ProductImage
 
 class ProductSerializer(serializers.ModelSerializer):
+    '''
+    Serializer para representar un producto, incluyendo sus imágenes.
+    '''
     images = serializers.SerializerMethodField()
 
     class Meta:
@@ -11,10 +14,22 @@ class ProductSerializer(serializers.ModelSerializer):
         fields = ['id', 'name', 'price', 'description', 'stock', 'category', 'images']
     
     def get_images(self, obj):
+        '''
+        Obtiene las imágenes del producto.
+        
+        Args:
+            obj (Product): La instancia del producto.
+        
+        Returns:
+            list: Una lista de diccionarios con los IDs y URLs de las imágenes.
+        '''
         images = obj.images.all()
         return [{'id': image.id, 'url': image.image.url} for image in images]
 
 class NewProductSerializer(serializers.ModelSerializer):
+    '''
+    Serializer para crear un nuevo producto.
+    '''
     category_id = serializers.PrimaryKeyRelatedField(queryset=Category.objects.all(), source='category')
     images = serializers.ListField(child=serializers.ImageField(), write_only=True)
 
@@ -22,24 +37,37 @@ class NewProductSerializer(serializers.ModelSerializer):
         model = Product
         fields = ['name', 'price', 'description', 'stock', 'category_id', 'images']
 
-    def validate_price(self, value):
-        if value < 0:
-            raise serializers.ValidationError(_("Price must be greater than 0"))
-        return value
-
-    def validate_stock(self, value):
-        if value < 0:
-            raise serializers.ValidationError(_("Stock must be greater than 0"))
-        return value
-
     def validate(self, attrs):
-        if attrs.get('name', "") == "":
-            raise serializers.ValidationError(_("Name must be provided"))
-        if attrs.get('description', "") == "":
-            raise serializers.ValidationError(_("Description must be provided"))
+        '''
+        Valida los atributos del producto.
+        Asegura que el precio y el stock no sean negativos.
+        
+        Args:
+            attrs (dict): Los datos validados.
+        
+        Returns:
+            dict: Los datos validados.
+        
+        Raises:
+            serializers.ValidationError: Si el precio o el stock son negativos.
+        '''
+        if 'price' in attrs and attrs['price'] < 0:
+            raise serializers.ValidationError(_('El precio debe ser mayor que 0'))
+        if 'stock' in attrs and attrs['stock'] < 0:
+            raise serializers.ValidationError(_('El stock debe ser mayor que 0'))
         return attrs
-    
+
     def create(self, validated_data):
+        '''
+        Crea una nueva instancia de producto con los datos validados.
+        Maneja la adición de imágenes.
+        
+        Args:
+            validated_data (dict): Los datos validados para crear el producto.
+        
+        Returns:
+            Product: La instancia del producto creada.
+        '''
         images = validated_data.pop('images', [])
         product = Product.objects.create(**validated_data)
         for image in images:
@@ -47,6 +75,9 @@ class NewProductSerializer(serializers.ModelSerializer):
         return product
 
 class UpdateProductSerializer(serializers.ModelSerializer):
+    '''
+    Serializer para actualizar una instancia de producto, incluyendo el manejo de imágenes.
+    '''
     category_id = serializers.PrimaryKeyRelatedField(queryset=Category.objects.all(), source='category', required=False)
     images_to_delete = serializers.ListField(child=serializers.IntegerField(), write_only=True, required=False)
     new_images = serializers.ListField(child=serializers.ImageField(), write_only=True, required=False)
@@ -57,24 +88,50 @@ class UpdateProductSerializer(serializers.ModelSerializer):
         partial = True
 
     def validate(self, attrs):
+        '''
+        Valida los atributos del producto.
+        Asegura que el precio y el stock no sean negativos.
+        
+        Args:
+            attrs (dict): Los datos validados.
+        
+        Returns:
+            dict: Los datos validados.
+        
+        Raises:
+            serializers.ValidationError: Si el precio o el stock son negativos.
+        '''
         if 'price' in attrs and attrs['price'] < 0:
-            raise serializers.ValidationError(_("Price must be greater than 0"))
+            raise serializers.ValidationError(_('El precio debe ser mayor que 0'))
         if 'stock' in attrs and attrs['stock'] < 0:
-            raise serializers.ValidationError(_("Stock must be greater than 0"))
+            raise serializers.ValidationError(_('El stock debe ser mayor que 0'))
         return attrs
 
     def update(self, instance, validated_data):
+        '''
+        Actualiza la instancia del producto con los datos validados proporcionados.
+        Maneja la adición y eliminación de imágenes.
+        
+        Args:
+            instance (Product): La instancia del producto a actualizar.
+            validated_data (dict): Los datos validados para actualizar el producto.
+        
+        Returns:
+            Product: La instancia del producto actualizada.
+        '''
         images_to_delete = validated_data.pop('images_to_delete', [])
         new_images = validated_data.pop('new_images', [])
 
-        # Update the product instance with the remaining validated data
+        # Actualiza la instancia del producto con los datos validados restantes
         for attr, value in validated_data.items():
             setattr(instance, attr, value)
         instance.save()
 
+        # Elimina las imágenes especificadas si las hay
         if images_to_delete:
             instance.images.filter(id__in=images_to_delete).delete()
 
+        # Añade nuevas imágenes si las hay
         if new_images:
             for image in new_images:
                 ProductImage.objects.create(product=instance, image=image)
--- backend/apps/product/views.py
+++ backend/apps/product/views.py
@@ -1,22 +1,57 @@
-from .models import Product
-from .serializers import ProductSerializer, NewProductSerializer, UpdateProductSerializer
-from rest_framework.parsers import MultiPartParser, FormParser
 from rest_framework.views import APIView
+from rest_framework.parsers import MultiPartParser, FormParser
 from rest_framework.response import Response
 from rest_framework import status
-from json import JSONDecodeError
+from .models import Product
+from .serializers import ProductSerializer, NewProductSerializer
+from json.decoder import JSONDecodeError
 
 class ProductView(APIView):
+    '''
+    Vista para manejar las operaciones CRUD de los productos.
+    '''
     parser_classes = [MultiPartParser, FormParser]
+
     def dispatch(self, request, *args, **kwargs):
+        '''
+        Sobrescribe el método dispatch para manejar la solicitud.
+        
+        Args:
+            request (HttpRequest): La solicitud HTTP.
+            *args: Argumentos adicionales.
+            **kwargs: Argumentos clave adicionales.
+        
+        Returns:
+            HttpResponse: La respuesta HTTP.
+        '''
         return super().dispatch(request, *args, **kwargs)
     
     def get(self, request):
-        products = Product.objects.all()
+        '''
+        Maneja las solicitudes GET para obtener todos los productos.
+        
+        Args:
+            request (HttpRequest): La solicitud HTTP.
+        
+        Returns:
+            Response: La respuesta HTTP con los datos de los productos.
+        '''
+        products = Product.objects.filter(status=True)
         serializer = ProductSerializer(products, many=True, context={'request': request})
         return Response(serializer.data)
     
     def post(self, request, *args, **kwargs):
+        '''
+        Maneja las solicitudes POST para crear un nuevo producto.
+        
+        Args:
+            request (HttpRequest): La solicitud HTTP.
+            *args: Argumentos adicionales.
+            **kwargs: Argumentos clave adicionales.
+        
+        Returns:
+            Response: La respuesta HTTP con los datos del producto creado o errores de validación.
+        '''
         try:
             serializer = NewProductSerializer(data=request.data)
             if serializer.is_valid():
@@ -24,20 +59,22 @@ class ProductView(APIView):
                 return Response(serializer.data, status=status.HTTP_201_CREATED)
             return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
         except JSONDecodeError:
-            return Response("Invalid JSON", status=status.HTTP_400_BAD_REQUEST)
-    
-    def patch(self, request, pk):
-        try:
-            product = Product.objects.get(id=pk)
-            serializer = UpdateProductSerializer(product, data=request.data, partial=True)  # Cambiar aquí
-            if serializer.is_valid():
-                serializer.save()
-                return Response(serializer.data)
-            return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
-        except Product.DoesNotExist:
-            return Response({"error": "Product not found"}, status=status.HTTP_404_NOT_FOUND)
+            return Response('JSON inválido', status=status.HTTP_400_BAD_REQUEST)
         
     def delete(self, request, pk):
-        product = Product.objects.get(id=pk)
-        product.update(status=False)
-        return Response("Product deleted", status=status.HTTP_204_NO_CONTENT)
+        '''
+        Maneja las solicitudes DELETE para eliminar un producto.
+        
+        Args:
+            request (HttpRequest): La solicitud HTTP.
+            pk (int): El ID del producto a eliminar.
+        
+        Returns:
+            Response: La respuesta HTTP con el resultado de la eliminación.
+        '''
+        product = Product.objects.filter(pk=pk).first()
+        if product:
+            product.status = False
+            product.save()
+            return Response('Producto eliminado', status=status.HTTP_204_NO_CONTENT)
+        return Response('Producto no encontrado', status=status.HTTP_404_NOT_FOUND)
