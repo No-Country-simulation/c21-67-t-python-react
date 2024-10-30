989bb307f927d45c2c196cd88e435d02f673aa04
Sol <solzito.1889@gmail.com>
2024-10-29 21:02:04 -0300

    se corrige api para utilizar la del back

--- Front-End/bytech/src/utils/config.ts
+++ Front-End/bytech/src/utils/config.ts
@@ -1,3 +1,3 @@
-const URL_BASE = "https://rickandmortyapi.com/api";
-
+//const URL_BASE = "https://rickandmortyapi.com/api";
+const URL_BASE = "http://localhost:8000/api";
 export const API_URL = `${URL_BASE}`;
